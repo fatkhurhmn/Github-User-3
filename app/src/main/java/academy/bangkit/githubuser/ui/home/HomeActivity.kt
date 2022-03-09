45d56746384f51aac2d3329df3aff68cebedb91a
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-03-09 20:34:10 +0700

    membuat toobar dan search view pada activity_home.xml

--- app/src/main/java/academy/bangkit/githubuser/ui/home/HomeActivity.kt
+++ app/src/main/java/academy/bangkit/githubuser/ui/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package academy.bangkit.githubuser
+package academy.bangkit.githubuser.ui.home
 
+import academy.bangkit.githubuser.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
