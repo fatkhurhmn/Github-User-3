4934050150e9d4cc7ffd78802e3aec7062fc21e5
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-03-10 16:14:31 +0700

    membuat UserAdapter.kt

---
+++ app/src/main/java/academy/bangkit/githubuser/adapter/UserAdapter.kt
@@ -0,0 +1,47 @@
+package academy.bangkit.githubuser.adapter
+
+import academy.bangkit.githubuser.databinding.UserItemBinding
+import academy.bangkit.githubuser.model.User
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+class UserAdapter : RecyclerView.Adapter<UserAdapter.ListViewHolder>() {
+
+    private var listUsers = ArrayList<User>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserAdapter.ListViewHolder {
+        val userItemBinding =
+            UserItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListViewHolder(userItemBinding)
+    }
+
+    override fun onBindViewHolder(holder: UserAdapter.ListViewHolder, position: Int) {
+        holder.bind(listUsers[position])
+    }
+
+    override fun getItemCount() = listUsers.size
+
+    inner class ListViewHolder(private val binding: UserItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User) {
+            with(binding) {
+                tvItemUsername.text = user.username
+                tvItemId.text = user.id.toString()
+                imgItemPhoto.loadImage(user.avatarUrl)
+            }
+        }
+
+    }
+
+    fun ImageView.loadImage(url: String) {
+        Glide.with(this.context)
+            .load(url)
+            .apply(RequestOptions().override(500, 500))
+            .centerCrop()
+            .into(this)
+    }
+}
