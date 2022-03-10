b67594af9f676f26ff640dd72e4cfa0fdad4e2d7
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-03-10 16:27:59 +0700

    membuat ApiConfig.kt dan ApiService.kt

---
+++ app/src/main/java/academy/bangkit/githubuser/network/ApiConfig.kt
@@ -0,0 +1,34 @@
+package academy.bangkit.githubuser.network
+
+import academy.bangkit.githubuser.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+    companion object {
+        private const val BASE_URL = "https://api.github.com/"
+        fun getApiService(): ApiService {
+            val loggingInterceptor = HttpLoggingInterceptor().setLevel(
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else {
+                    HttpLoggingInterceptor.Level.NONE
+                }
+            )
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+
+            return retrofit.create(ApiService::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/academy/bangkit/githubuser/network/ApiService.kt
@@ -0,0 +1,4 @@
+package academy.bangkit.githubuser.network
+
+interface ApiService {
+}
