b2bc473e1695d29ca1991083157fafe34e54da7c
Muhammad Fatkhu Rohman <fatkhurohman019@gmail.com>
2022-03-15 21:35:17 +0700

    hapus import yang tidak digunakan

--- app/src/main/java/academy/bangkit/githubuser/network/ApiService.kt
+++ app/src/main/java/academy/bangkit/githubuser/network/ApiService.kt
@@ -1,11 +1,13 @@
 package academy.bangkit.githubuser.network
 
-import academy.bangkit.githubuser.BuildConfig
 import academy.bangkit.githubuser.model.SearchResponse
 import academy.bangkit.githubuser.model.User
 import academy.bangkit.githubuser.model.UserDetail
 import retrofit2.Call
-import retrofit2.http.*
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
     @GET("/search/users")
